ac9b85aa0f82c09df7a9caa8c2f88d2064069695
Matt Roszyk <matt@roszyk.com>
2025-09-18 16:39:20 -0500

    TBS-005: Completed the initial addition of the API module.

---
+++ template/module-api/lib/Say.class.php
@@ -0,0 +1,14 @@
+<?='<?php'?>
+
+namespace BoomStick\Module\<?=O::$moduleNamespace;?>\Lib;
+use BoomStick\Lib\Debug as D;
+
+class Say
+{
+	public static function hello()
+	{
+		return (object)[
+			'hello' => 'From the API module'
+		];
+	}
+}
