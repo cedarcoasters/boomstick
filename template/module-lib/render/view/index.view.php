956deca7f4bacefcd2715a4c163d5fb9bf4bc3e2
Matt Roszyk <matt@roszyk.com>
2025-09-18 16:51:06 -0500

    TBS-004: Added the share library module;

--- template/module-lib/render/view/index.view.php
+++
@@ -1,9 +0,0 @@
-<?='<?=';?>$this->insertStyle('index');?>
-<div class="container">
-	<div class="row">
-		<div class="col text-center mt-5">
-			<h1>Hello from <span class="blink text-warning">BoomStick</span> (index[view])</h1>
-		</div>
-	</div>
-</div>
-<?='<?=';?>$this->insertScript('index');?>
